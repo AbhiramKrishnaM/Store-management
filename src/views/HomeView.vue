09ac7412ec52222bc530d5627eb8acc084d02a53
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-01-02 14:03:53 +0530

    feat: added navigation bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <h1>hello world</h1>
-    <span
-      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi
+    <span>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi
       repudiandae, placeat sed incidunt quis nemo perferendis suscipit, quae
       veniam aspernatur quaerat, dolorum ducimus vel libero modi. Hic eum nihil
       odit accusantium debitis quo dolore. Doloremque reprehenderit id magni
@@ -12,7 +12,7 @@
       quia modi minima odit nisi assumenda ea recusandae neque dignissimos amet!
       Quibusdam facere aliquid a ea natus inventore consectetur praesentium
       sapiente consequuntur similique. Veritatis nihil deserunt repudiandae
-      saepe, mollitia excepturi a soluta reiciendis possimus?</span
-    >
+      saepe, mollitia excepturi a soluta reiciendis possimus?
+    </span>
   </main>
 </template>
