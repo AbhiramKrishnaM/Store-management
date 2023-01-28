f8432e3b7c259f72b50828c348e3767c686c86bb
Harikrishnan <hknp796@gmail.com>
2023-01-28 12:35:30 +0530

    user avatar card added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
 import Login from "@/components/section/login/index.vue";
 import CustomerCard from "../components/section/customerCard.vue";
 import ProductCard from "../components/section/productCard.vue";
-
+import UserAvatarInfo from "../components/section/userAvatarInfo.vue";
 const customerCard = {
   image: {
     path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoEprVQayH51WidWqyN8jnoaAczBilZFNPg0ePJFywkerljh-CwI7Jkt_sJinuS0A5qxQ&usqp=CAU",
@@ -27,12 +27,25 @@ const productCard = {
     discountedAmount: 40,
   },
 };
+
+const customerInfo = {
+  image: {
+    path: "https://png.pngtree.com/png-vector/20220817/ourmid/pngtree-cartoon-man-avatar-vector-ilustration-png-image_6111064.png",
+    alt: "avatar",
+  },
+  username: "Ahamed-Geidt",
+  address: "Montreal, Canada",
+  product: {
+    quantity: 30,
+  },
+  customer: {
+    count: 35.4,
+  },
+};
 </script>
 
 <template>
   <div>
-    <Login />
-    <CustomerCard v-bind="customerCard" />
-    <ProductCard v-bind="productCard" />
+    <UserAvatarInfo v-bind="customerInfo" />
   </div>
 </template>
