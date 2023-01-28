a018bd96388612ff4d865941f87d6302481e06e0
Harikrishnan <hknp796@gmail.com>
2023-01-28 13:12:20 +0530

    special package added

---
+++ src/components/section/specialPackage.vue
@@ -0,0 +1,28 @@
+<template lang="">
+  <div>
+    <div class="flex bg-red-50 p-10 rounded-xl">
+      <div class="mr-2">
+        <h3 class="font-bold mb-3">{{ title }}</h3>
+        <p>
+          {{ subtitle }}
+        </p>
+        <button
+          type="button"
+          class="mt-3 rounded-lg bg-blue-500 px-5 py-2.5 font-medium text-white"
+        >
+          Manage Package
+        </button>
+      </div>
+      <div>
+        <img :src="image.path" :alt="image.alt" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  image: Object,
+  title: String,
+  subtitle: String,
+});
+</script>
