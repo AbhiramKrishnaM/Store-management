c5960cb5a55acbfa415018edacd86d369c5fe591
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-02 21:24:57 +0530

    added test route, working

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 
+// lib
 const express = require("express");
 
+// routes
+const test = require("./routes/Test.js");
+
+// server
 const app = express();
 
 const bodyParser = require("body-parser");
@@ -10,7 +15,10 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 4001;
+// use routes
+app.use("/test", test);
+
+const port = process.env.APP_PORT || 4001;
 
 app.listen(port, () => {
   console.log("Application listening on port ", port);
