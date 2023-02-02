d4b85566bd24b485e9353e720fd55003bb86d4eb
Harikrishnan <hknp796@gmail.com>
2023-02-02 19:43:33 +0530

    modal is created with login form

---
+++ src/components/core/Modal.vue
@@ -0,0 +1,77 @@
+<template>
+  <button
+    type="button"
+    @click="openModal"
+    class="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+  >
+    Open dialog
+  </button>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <slot></slot>
+              <!-- slot for forms -->
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 absolute top-2 right-2"
+                  @click="closeModal"
+                >
+                  <Icon icon="ic:outline-close" />
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup>
+import { ref, defineExpose } from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
+
+const isOpen = ref(true);
+
+function closeModal() {
+  isOpen.value = false;
+}
+function openModal() {
+  isOpen.value = true;
+}
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Login from "../components/section/login/index.vue";
+import Modal from "../components/core/Modal.vue";
 </script>
 
 <template>
   <div>
-    <Login />
+    <Modal>
+      <template v-slot:default>
+        <Login />
+      </template>
+    </Modal>
   </div>
 </template>
