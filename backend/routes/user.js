47b8470a93f1cfe5293ff9ddcdda82a1eddb401d
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-03 23:31:52 +0530

    small change for user router import

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 
-const router = express.router();
+const router = express.Router();
 
 router.post("/register", (req, res) => {
   res.send("User registration end");
