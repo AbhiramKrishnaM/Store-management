18310460af9c1b4b6b0cd8f46d37fc4d83278b04
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-05 16:19:13 +0530

    added readme file, and added test router post

--- backend/routes/Test.js
+++ backend/routes/Test.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/get", (req, res) => {
   res.send("Hey there!!");
+});
+
+router.post("/post", (req, res) => {
+  res.send(`Hello ${req.body.name}! nice to meet you.`);
 });
 
 module.exports = router;
