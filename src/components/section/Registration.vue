7cea05a1fc29cb0ff5254bd050a58e0583e1e304
Harikrishnan <hknp796@gmail.com>
2023-02-05 17:21:50 +0530

    registration page added

---
+++ src/components/section/Registration.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="md:flex h-screen md:items-center md:justify-center">
+    <div class="lg:w-3/12">
+      <div class="mb-10 flex text-lg font-bold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="mr-3"
+          width="32"
+          height="32"
+          viewBox="0 0 512 512"
+        >
+          <path
+            fill="currentColor"
+            d="M255.5 15c-132 0-240 108-240 240s108 240 240 240c85.4 0 160.8-45.2 203.3-112.9a6.87 6.87 0 0 0-9.1-9.7a108.64 108.64 0 0 1-18.4 8.6c-36.8 12.6-57.1 13.1-82.1 12c-27.9-1.2-61.9-10.8-85.8-25s-43.5-34.6-54.1-52.3s-17-39.9-14.1-68.3c2.9-29 29.4-52.6 60.4-52.6c33.5 0 60.8 26.6 60.8 60.1c0 17-8.1 31.7-18.5 43.5c-2.3 2.1-7.6 9.7 5.8 20c15.9 12.2 51.6 18 79.9 16.6s59.1-12.6 80.2-34.8c16.8-17.7 31.8-46.1 31.8-77.4C495.5 97.7 379.5 15 255.5 15Z"
+          />
+        </svg>
+        Blue Reciept
+      </div>
+      <div>
+        <h6 class="mb-3 text-xl">Create your account</h6>
+        <h6 class="mb-8 text-sm">
+          Increase ecommerce revenue by 15-30% with conversational commerce
+        </h6>
+      </div>
+
+      <form>
+        <div class="mb-6">
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >First Name</label
+          >
+          <input
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Last Name</label
+          >
+          <input
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+        </div>
+        <div class="mb-10">
+          <label
+            for="repeat-password"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Email</label
+          >
+          <input
+            type="email"
+            id="repeat-password"
+            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          class="flex justify-center align-middle text-white bg-blue-700 font-medium rounded-lg text-sm text-center w-full py-2"
+        >
+          Continue
+          <Icon icon="ic:outline-arrow-right-alt" class="ml-3" width="20" />
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
---
+++ src/views/Registration.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Registration />
+  </div>
+</template>
+
+<script>
+import Registration from "../components/section/Registration.vue";
+export default {
+  components: { Registration },
+};
+</script>
